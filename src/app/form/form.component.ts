e2202fc8904412b79571507a139110c0efd7ec67
shayanrj2000 <shayanrj2000@gmail.com>
2023-11-26 11:48:29 +0330

    data binding to the form is now working

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  firstName: string;
+  lastName: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addUser() {
+    console.log('First Name:', this.firstName);
+    console.log('Last Name:', this.lastName);
+    this.firstName = '';
+    this.lastName = '';
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
